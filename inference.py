e972a36177dd267532fd33811afeea7704e907be
aufr33 <65520685+aufr33@users.noreply.github.com>
2024-05-22 10:34:48 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -17,6 +17,51 @@ from utils import demix_track, demix_track_demucs, get_model_from_config
 import warnings
 warnings.filterwarnings("ignore")
 
+def run_single_file(model, args, config, device, verbose=False):
+    start_time = time.time()
+    model.eval()
+
+    if not os.path.isfile(args.input_file):
+        time.sleep(1)
+        if not os.path.isfile(args.input_file):
+            print("Input file doesn't exist!")
+            return
+
+    if not os.path.isdir(args.store_dir):
+        os.mkdir(args.store_dir)
+
+    try:
+        mix, sr = librosa.load(args.input_file, sr=44100, mono=False)
+        mix = mix.T
+        original_length = mix.shape[0]
+        
+        # Adding 5 seconds of silence to fix the bug
+        silence_duration = 5 
+        silence = np.zeros((silence_duration * sr, mix.shape[1]))
+        mix = np.vstack([mix, silence])
+    except Exception as e:
+        print('Can read track: {}'.format(args.input_file))
+        print('Error message: {}'.format(str(e)))
+        return
+
+    # Convert mono to stereo if needed
+    if len(mix.shape) == 1:
+        mix = np.stack([mix, mix], axis=-1)
+
+    mixture = torch.tensor(mix.T, dtype=torch.float32)
+    if args.model_type == 'htdemucs':
+        res = demix_track_demucs(config, model, mixture, device)
+    else:
+        res = demix_track(config, model, mixture, device)
+
+    instruments = config.training.instruments
+    if config.training.target_instrument is not None:
+        instruments = [config.training.target_instrument]
+
+    for instr in instruments:
+        res[instr] = res[instr][:, :original_length] # Removing the last 5 seconds of silence
+        sf.write("{}/{}_{}.wav".format(args.store_dir, os.path.basename(args.input_file)[:-4], instr), res[instr].T, sr, subtype='FLOAT')
+
 
 def run_folder(model, args, config, device, verbose=False):
     start_time = time.time()
@@ -66,12 +111,13 @@ def run_folder(model, args, config, device, verbose=False):
     print("Elapsed time: {:.2f} sec".format(time.time() - start_time))
 
 
-def proc_folder(args):
+def proc_single_file(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_type", type=str, default='mdx23c', help="One of mdx23c, htdemucs, segm_models, mel_band_roformer, bs_roformer, swin_upernet, bandit")
     parser.add_argument("--config_path", type=str, help="path to config file")
     parser.add_argument("--start_check_point", type=str, default='', help="Initial checkpoint to valid weights")
-    parser.add_argument("--input_folder", type=str, help="folder with mixtures to process")
+    #parser.add_argument("--input_folder", type=str, help="folder with mixtures to process")
+    parser.add_argument("--input_file", type=str, help="path to input audio file")
     parser.add_argument("--store_dir", default="", type=str, help="path to store results as wav file")
     parser.add_argument("--device_ids", nargs='+', type=int, default=0, help='list of gpu ids')
     parser.add_argument("--extract_instrumental", action='store_true', help="invert vocals to get instrumental if provided")
@@ -106,8 +152,8 @@ def proc_folder(args):
         print('CUDA is not avilable. Run inference on CPU. It will be very slow...')
         model = model.to(device)
 
-    run_folder(model, args, config, device, verbose=False)
+    run_single_file(model, args, config, device, verbose=False)
 
 
 if __name__ == "__main__":
-    proc_folder(None)
+    proc_single_file(None)
